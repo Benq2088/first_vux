485347e026834be9fe991379d05443a7b59feab6
Benq2088 <31240784+Benq2088@users.noreply.github.com>
2017-08-27 21:48:07 +0800

    路由分离，单独分开放入config-router.js中

---
+++ vux/src/config-router.js
@@ -0,0 +1,104 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/', // 微信
+      component: function (resolve) {
+        require(['./tmp/WeChat.vue'], resolve)
+      }
+    },
+    /**
+      * 通讯录
+      */
+    {
+      path: '/concat',
+      component: function (resolve) {
+        require(['./tmp/Concat.vue'], resolve)
+      }
+    },
+    {
+      path: '/concat/tree',
+      component: function (resolve) {
+        require(['./tmp/concat/Tree.vue'], resolve)
+      }
+    },
+    {
+      path: '/personCenter',
+      component: function (resolve) {
+        require(['./tmp/PersonalCenter.vue'], resolve)
+      }
+    },
+    {
+      path: '/personCenter/acitivity',
+      component: function (resolve) {
+        require(['./tmp/PersonalCenter/Acitivity.vue'], resolve)
+      }
+    },
+    {
+      path: '/personCenter/concat',
+      redirect: '/personCenter/concat',
+      component: function (resolve) {
+        require(['./tmp/PersonalCenter/Concat.vue'], resolve)
+      },
+      children: [
+        {
+          path: '/personCenter/concat/all',
+          component: function (resolve) {
+            require(['./tmp/PersonalCenter/concat/All.vue'], resolve)
+          }
+        },
+        {
+          path: '/personCenter/concat/judge',
+          component: function (resolve) {
+            require(['./tmp/PersonalCenter/concat/Judge.vue'], resolve)
+          }
+        },
+        {
+          path: '/personCenter/concat/judged',
+          component: function (resolve) {
+            require(['./tmp/PersonalCenter/concat/Judged.vue'], resolve)
+          }
+        },
+        {
+          path: '/personCenter/concat/handing',
+          component: function (resolve) {
+            require(['./tmp/PersonalCenter/concat/Handing.vue'], resolve)
+          }
+        }
+      ]
+    },
+    /**
+      * 发现
+      */
+    {
+      path: '/find',
+      component: function (resolve) {
+        require(['./tmp/Find.vue'], resolve)
+      }
+    },
+    /**
+      * 我
+      */
+    {
+      path: '/me',
+      component: function (resolve) {
+        require(['./tmp/Me.vue'], resolve)
+      }
+    },
+    {
+      path: '/me.purse/:id', // 参数传递，跟ionic类似,取时使用this.$route.params.id
+      component: function (resolve) {
+        require(['./tmp/me/Purse.vue'], resolve)
+      }
+    },
+    {
+      path: '/me.setting',
+      component: function (resolve) {
+        require(['./tmp/me/Setting.vue'], resolve)
+      }
+    }
+  ]
+})
--- vux/src/main.js
+++ vux/src/main.js
@@ -1,13 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
-import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 import App from './App'
 import vuexI18n from 'vuex-i18n'
-Vue.use(VueRouter)
+import router from './config-router'
 
 let store = new Vuex.Store({
   modules: {
@@ -53,107 +50,7 @@ if (process.env.platform === 'app') {
   Vue.use(AppPlugin, store)
 }
 
-const routes = [{
-  path: '/', // 微信
-  component: function (resolve) {
-    require(['./tmp/WeChat.vue'], resolve)
-  }
-},
-/**
-  * 通讯录
-  */
-{
-  path: '/concat',
-  component: function (resolve) {
-    require(['./tmp/Concat.vue'], resolve)
-  }
-},
-{
-  path: '/concat/tree',
-  component: function (resolve) {
-    require(['./tmp/concat/Tree.vue'], resolve)
-  }
-},
-{
-  path: '/personCenter',
-  component: function (resolve) {
-    require(['./tmp/PersonalCenter.vue'], resolve)
-  }
-},
-{
-  path: '/personCenter/acitivity',
-  component: function (resolve) {
-    require(['./tmp/PersonalCenter/Acitivity.vue'], resolve)
-  }
-},
-{
-  path: '/personCenter/concat',
-  redirect: '/personCenter/concat',
-  component: function (resolve) {
-    require(['./tmp/PersonalCenter/Concat.vue'], resolve)
-  },
-  children: [
-    {
-      path: '/personCenter/concat/all',
-      component: function (resolve) {
-        require(['./tmp/PersonalCenter/concat/All.vue'], resolve)
-      }
-    },
-    {
-      path: '/personCenter/concat/judge',
-      component: function (resolve) {
-        require(['./tmp/PersonalCenter/concat/Judge.vue'], resolve)
-      }
-    },
-    {
-      path: '/personCenter/concat/judged',
-      component: function (resolve) {
-        require(['./tmp/PersonalCenter/concat/Judged.vue'], resolve)
-      }
-    },
-    {
-      path: '/personCenter/concat/handing',
-      component: function (resolve) {
-        require(['./tmp/PersonalCenter/concat/Handing.vue'], resolve)
-      }
-    }
-  ]
-},
-/**
-  * 发现
-  */
-{
-  path: '/find',
-  component: function (resolve) {
-    require(['./tmp/Find.vue'], resolve)
-  }
-},
- /**
-  * 我
-  */
-{
-  path: '/me',
-  component: function (resolve) {
-    require(['./tmp/Me.vue'], resolve)
-  }
-},
-{
-  path: '/me.purse/:id', // 参数传递，跟ionic类似,取时使用this.$route.params.id
-  component: function (resolve) {
-    require(['./tmp/me/Purse.vue'], resolve)
-  }
-},
-{
-  path: '/me.setting',
-  component: function (resolve) {
-    require(['./tmp/me/Setting.vue'], resolve)
-  }
-}
-]
 
-const router = new VueRouter({
-  routes
-})
 
 FastClick.attach(document.body)
 
